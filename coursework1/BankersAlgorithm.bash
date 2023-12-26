99327da971d9d924e8431df07ec56b0cac778034
MrRincon <ar1564@live.mdx.ac.uk>
2023-12-26 07:42:29 +0000

    processors an resources input check

--- coursework1/BankersAlgorithm.bash
+++ coursework1/BankersAlgorithm.bash
@@ -1,2 +1,41 @@
 #!/bin/bash
 #BankersAlgorithm will happen in here
+#Bankers Algorithm for Deadlock Avoidance using Bash
+start(){
+#Check that inputs are correct
+    getProcessors
+    echo "There is $numProcessors processor(s)"
+    getResources
+    echo "There is $numResources resource(s)"
+}
+getProcessors(){
+    read -p "Enter the number of processors(min 1 and max 5): " numProcessors
+    if [ -z $numProcessors ] 
+    then
+        # echo "Please enter some processors"
+        getProcessors
+    elif [ $numProcessors -lt 1 ] || [ $numProcessors -gt 5 ] 
+    then
+        # echo "Minimum 1 and maximum 10 processors. try again"
+        getProcessors
+    else 
+        # echo "There is $numProcessors processor(s)"
+        return $numProcessors
+    fi
+}
+getResources(){
+    read -p "Enter the number of resources: " numResources
+    if [ -z $numResources ] 
+    then
+        # echo "Please enter some resources"
+        getResources 
+    elif [ $numResources -lt 1 ] || [ $numResources -gt 3 ] 
+    then
+        # echo "Minimum 1 and maximum 3 resources. try again"
+        getResources
+    else 
+        # echo "There is $numResources resources(s)"
+        return $numResources
+    fi
+}
+start
